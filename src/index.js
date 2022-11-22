8302c7348a8b5266606e94b4c958c814b6ae2d99
Levi Orton <levi.orton@alex.com.au>
2022-11-22 12:46:05 +1000

    retry later

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
-
-
-export default function Button() {
+function MyButton() {
     return (
       <button>
-        I don't do anything
+        I'm a button
       </button>
     );
   }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  
