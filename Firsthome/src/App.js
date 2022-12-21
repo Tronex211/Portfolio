ad91841b8b7e07090c7ec7db31b9a737baecf2e9
Levi Orton <levi.orton@alex.com.au>
2022-12-21 10:18:59 +1000

    first commmit

---
+++ Firsthome/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './TicTacToe';
+import { LinkHTMLAttributes } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="/TicTacToe.js"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
